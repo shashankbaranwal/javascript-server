1dc9ac3da55c866559e07bd67de6028b0191f02f
Shashank Baranwal <shashank.baranwal@tr01.successivetech.com>
2020-10-15 17:23:44 +0530

    docs: Add git doc file

---
+++ extra/patterns/equilateral.js
@@ -0,0 +1,15 @@
+function createequilatral(n)
+{
+    // using for white space
+    let s = "";
+    // using for rows
+    let j = n;
+    for(let i=1;i<=n;i++){
+        s=" ".repeat(j);
+        console.log(s,'* '.repeat(i));
+        s="";
+        j--;
+    }
+}
+
+createequilatral(10)
