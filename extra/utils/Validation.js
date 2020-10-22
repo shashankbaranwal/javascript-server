8c84843f93b05d480c495a778fa293955b7791cd
Shashank Baranwal <shashank.baranwal@tr01.successivetech.com>
2020-10-22 15:43:22 +0530

    Resolve issues

--- extra/utils/Validation.js
+++ extra/utils/Validation.js
@@ -9,11 +9,14 @@ let users = [
     }
 ]
 
-/* function validateEmail(email){
+let a = [];
+let b = [];
+
+/*function validateEmail(email){
     const regix = /\w+.\w+@successive.tech$/i;
     return regix.test(email)
-}
-*/
+}*/
+
 function validateEmail(email){
     const regex = /\w+.\w+@successive.tech$/i;
     if(regex.test(email))
@@ -30,21 +33,26 @@ function validateUsers(users){
     let countValid=0;
     let countInvalid=0;
     users.forEach(item => {
-        const {TraineeEmail}= item;
-        const {ReviewerEmail}= item;
+        const {TraineeEmail,ReviewerEmail}= item;
         if(validateEmail(TraineeEmail)){
-          countValid++;
-        }
-        else{
-            countInvalid++;
-        }
-        if(validateEmail(ReviewerEmail)){
+            a.push(TraineeEmail);
             countValid++;
         }
         else{
+            b.push(TraineeEmail);
+            countInvalid++;
+        }
+        if(validateEmail(ReviewerEmail)){
+            a.push(ReviewerEmail);
+            countValid++;
+        }
+        else{
+            b.push(ReviewerEmail);
             countInvalid++;
         }
     })
+    console.log("Valid is :"+a);
+    console.log("invalid is :" +b);
     console.log("Valid: "+countValid+" Invalid: "+countInvalid);
 };
     
