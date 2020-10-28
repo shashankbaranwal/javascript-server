e31122afe653c6f5ab5b9f4dca467c1176012263
shashankbaranwal <shashank.baranawal@successive.tech>
2020-10-28 11:46:05 +0530

    feat: fix all

--- extra/Arrays/indexOf.js
+++
@@ -1,9 +0,0 @@
-// Initialize the Arrays List
-
-let Users = ["Shashank", "gfffs", "ttter", "hhhhhd", "ggsjdj"];
-
-//Initialize indexOf method
-
-console.log(Users.indexOf('Shashank'));
-
-console.log(Users.indexOf('Shashank',2));
--- extra/Arrays/toSource.js
+++
@@ -1,9 +0,0 @@
-function employee(name,jobtitle,born)
-{
-    this.name=name;
-    this.jobtitle=jobtitle;
-    this.born=born;
-}
-let fred = new employee("Fred Flintstone","Caveman",1970);
-
-console.log(fred.toSource());
