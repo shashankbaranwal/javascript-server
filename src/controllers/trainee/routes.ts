f045252c245a484596c84c485ffcc4502793e491
Shashank Baranwal <72746365+shashankbaranwal@users.noreply.github.com>
2020-11-23 17:12:06 +0530

    Update routes.ts

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -5,9 +5,8 @@ import { validationHandler } from '../../libs/routes/validationHandler';
 import config from './validation';
 const traineeRouter = Router();
 traineeRouter.route('/')
-    .get(validationHandler(config.get))
-    .post(validationHandler(config.create))
-    .put(validationHandler(config.update), TraineeController.put)
-    .delete(validationHandler(config.delete), TraineeController.delete);
-
+    .get(authMiddleWare('getUsers', 'read'), validationHandler(config.get), TraineeController.get)
+    .post(authMiddleWare('getUsers', 'write'), validationHandler(config.create), TraineeController.create)
+    .put(authMiddleWare('getUsers', 'all'), validationHandler(config.update), TraineeController.update)
+    .delete(authMiddleWare('getUsers', 'delete'), validationHandler(config.Delete), TraineeController.delete);
 export default traineeRouter;
